feb6fba65a6552f7c8d349df5ede553782619763
TheUntitledGoose <aleksandr2008.lomin@gmail.com>
2025-08-17 22:14:46 -0500

    Alpha v0.1.2: Make the GUI update answers in some edge-cases.

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ Paste the **script.js** in a new script tab and go to a problem.
 I haven't found any issues other than the possibility of the UI going off-screen, so just.. don't have a big homework assignment
 
 I have no clue if this works on quizzes or anything else.
+
+
+## side note
+
+can i point out that, the website legit sends you the answers even if no check answers is active. i lowkey think this will work on like normal tests and quizzes and maybe even actual ap tests
+
+can someone go sacrifice their future to test this out 🥰
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // @name         Basic Answerer Library List Sorter  (BALLS)
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  
+// @description
 // @author       You
 // @match        https://apclassroom.collegeboard.org/*
 // @grant        unsafeWindow
@@ -34,7 +34,7 @@ window.addEventListener("message", (event) => {
     if (event.origin.includes("items-va")) {
         if(!msg.status) return
         console.log("[DEV] Passed interception check")
-        
+
         /*
         {
             message: 'QUESTION 1',
@@ -64,57 +64,62 @@ window.addEventListener("message", (event) => {
             }
         }
 
+        let c;
+
         if (!document.getElementById("myCanvas")) {
-            const c = document.createElement("canvas");
+            c = document.createElement("canvas");
             c.id = "myCanvas";
             c.style = "position: absolute; top: 0px; left: 0px; z-index: 1000;";
             document.body.appendChild(c);
-            
-            // initiate my imgui
-            const ctx = c.getContext("2d");
 
             imgui = new unsafeWindow.ImGui(200, 250, 800, 100, c);
             c.width = unsafeWindow.outerWidth;
             c.height = unsafeWindow.outerHeight;
             imgui.staticText("Answers:")
-            // imgui.init();
-
-            console.log(answers)
-            for (let a = 0; a < answers.length; a++) {
-                const answer = answers[a];
-                imgui.staticText(`${answer.message} ${answer.correct_answer}`);
-            }
-            imgui.init()
-            imgui.width = 300
-
-
-            function animate() {
-                ctx.clearRect(0, 0, c.width, c.height);
-
-                if (document.querySelector('.current-item-pos')) {
-                    const question_id = parseInt(document.querySelector('.current-item-pos').textContent.trim().split(' ')[0]);
-                    
-                    for (let a = 0; a < answers.length; a++) {
-                        const answer = answers[a];
-                        imgui.elements[a].text = question_id == (a+1) ? `> ${answer.message} ${answer.correct_answer}` : `${answer.message} ${answer.correct_answer}`;
-                        imgui.elements[a].color = question_id == (a+1) ? "green" : "white";
-                    }
-                }
-
-                imgui.draw();
-
-                unsafeWindow.requestAnimationFrame(animate);
-            }
-            unsafeWindow.requestAnimationFrame(animate);
-
-            document.addEventListener('mousemove', (e) => {
-                if (imgui.checkHover(e.x, e.y)) {
-                    c.style.pointerEvents = 'auto'; // Enable interaction with the canvas
-                } else {
-                    c.style.pointerEvents = 'none'; // Let mouse events pass through to HTML elements
-                }
-            })
         }
+        c = document.querySelector('#myCanvas')
+
+        // initiate my imgui
+        const ctx = c.getContext("2d");
+
+        imgui.elements = [];
+        // imgui.init();
+
+        console.log(answers)
+        for (let a = 0; a < answers.length; a++) {
+            const answer = answers[a];
+            imgui.staticText(`${answer.message} ${answer.correct_answer}`);
+        }
+        imgui.init()
+        imgui.width = 300
+
+
+        function animate() {
+            ctx.clearRect(0, 0, c.width, c.height);
+
+            if (document.querySelector('.current-item-pos')) {
+                const question_id = parseInt(document.querySelector('.current-item-pos').textContent.trim().split(' ')[0]);
+
+                for (let a = 0; a < answers.length; a++) {
+                    const answer = answers[a];
+                    imgui.elements[a].text = question_id == (a+1) ? `> ${answer.message} ${answer.correct_answer}` : `${answer.message} ${answer.correct_answer}`;
+                    imgui.elements[a].color = question_id == (a+1) ? "green" : "white";
+                }
+            }
+
+            imgui.draw();
+
+            unsafeWindow.requestAnimationFrame(animate);
+        }
+        unsafeWindow.requestAnimationFrame(animate);
+
+        document.addEventListener('mousemove', (e) => {
+            if (imgui.checkHover(e.x, e.y)) {
+                c.style.pointerEvents = 'auto'; // Enable interaction with the canvas
+            } else {
+                c.style.pointerEvents = 'none'; // Let mouse events pass through to HTML elements
+            }
+        })
 
     }
 }, true);
